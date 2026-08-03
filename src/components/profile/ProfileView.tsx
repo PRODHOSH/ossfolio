dfd3edcf7775dc68520f6466b69425a6cea9461c
Aditya8369 <mahajan2005aditya@gmail.com>
2026-08-03 16:18:15 +0530

    feat: Interactive Contribution Timeline

--- src/components/profile/ProfileView.tsx
+++ src/components/profile/ProfileView.tsx
@@ -2470,7 +2470,12 @@ export function ProfileView({
             exit={tabExit}
           >
             {/* Contribution Timeline */}
-            <ContributionTimeline mergedPRs={mergedPRs} badges={badgesList} />
+            <ContributionTimeline
+              mergedPRs={mergedPRs}
+              repos={repos}
+              orgs={orgs}
+              badges={badgesList}
+            />
           </motion.div>
         )}
 
